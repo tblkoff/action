adcf3af264819c4281830254dec97751e08950fe
tblkoff <rdk@rdk.rdk>
2023-11-14 20:52:05 +0400

    docs action bug fix

--- action/main.py
+++ action/main.py
@@ -2,7 +2,7 @@ from .__version__ import __version__
 
 
 def get_version():
-    """Get current lib`s version
+    """Get ``current`` lib`s version
 
     :return: The current lib`s version
     :rtype: str
